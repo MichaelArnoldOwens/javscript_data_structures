d7a9a92a820ee955f554a9a6f5f69f2725a87f39
Michael Owens <mowens@ucdavis.edu>
2015-05-30 22:47:57 -0700

    dequeue()

--- queue.js
+++ queue.js
@@ -4,7 +4,64 @@ function Queue() {
   var head = null;
   var tail = null;
 
+  //#getCount
   this.getCount = function() {
     return count;
+  };
+
+  //#isEmpty
+  this.isEmpty = function() {
+    if(count == 0) {
+      return true;
+    };
+    return false;
   }
+
+  //#enqueue
+  this.enqueue = function (data) {
+    
+    var node = {
+      data: data,
+      next: head
+    };
+    
+    if(this.isEmpty()) {
+      tail = node;
+    };
+
+    head = node;
+
+    count++;
+  };
+
+  // #dequeue
+  this.dequeue = function () {
+    if (this.isEmpty()) {
+      return null;
+    };
+
+    if (count > 0) {
+      count --;
+    }
+    else {
+      head = null;
+      tail = null;
+    }
+    
+    //find the second to last node
+    //alternative way is to have a previous
+    var current = head;
+    var previous = null;
+
+    while(current.next) {
+      previous = current;
+      current = current.next;
+    }
+    
+    //remove second to last reference
+    if(count != 1) {
+      previous.next = null
+    }
+  };
+
 };
